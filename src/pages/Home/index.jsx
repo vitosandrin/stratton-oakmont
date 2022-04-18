cb3c91205e238a5b3a553e7689d7963869ad1ed3
Vito Sandrin <93977494+vitosandrin@users.noreply.github.com>
2022-04-18 17:16:55 -0300

    Update index.jsx

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -28,7 +28,7 @@ export const Home = () => {
   const hasCoins = coins.length > 0;
 
   useEffect(() => {
-    api.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false')
+    api.get('')
       .then(res => {
         setCoins(res.data);
       }).catch(error => console.log(error))
